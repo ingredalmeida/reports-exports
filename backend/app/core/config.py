15506616052e297f27e5ee059edc4d41407e65ce
ingredalmeida1 <ingred.almeida@ufv.br>
2026-03-26 09:28:20 -0300

    feat: db configuration and session management

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,26 @@
-'''
-Configuração via environment variables usando pydantic-settings. Exemplo:
+from pydantic_settings import BaseSettings
 
-DATABASE_URL
-REDIS_URL
 
-'''
+class Settings(BaseSettings):
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    REDIS_PORT: int
+
+    DB_HOST: str = "localhost"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+
+def get_database_url() -> str:
+    return (
+        f"postgresql+psycopg://{settings.DB_USER}:"
+        f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
+        f"{settings.DB_PORT}/{settings.DB_NAME}"
+    )
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,3 +1,26 @@
-# Responsável por abrir conexões com Postgres.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
-# vai usar engine do sqlalchemy?
+from app.core.config import get_database_url
+
+DATABASE_URL = get_database_url()
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
