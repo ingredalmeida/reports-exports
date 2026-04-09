3a2147d83a3612f59e22ee2924644410c192421f
ingredalmeida1 <ingred.almeida@ufv.br>
2026-04-09 20:50:18 -0300

    wip: user services

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -7,5 +7,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 class User(BaseModel):
     __tablename__ = "users"
 
+    username: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
